c27b38f1307017ae44c00cad86beef4012906929
Kate Stulova <stulova.ek@gmail.com>
2021-11-22 23:27:14 +0300

    9. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -4,9 +4,11 @@ import java.time.format.DateTimeFormatter;
 
 public class StartUI {
     public static void main(String[] args) {
-        Item item = new Item();
+        Item item1 = new Item();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        String currentDateTimeFormat = item.getDateTime().format(formatter);
+        String currentDateTimeFormat = item1.getDateTime().format(formatter);
         System.out.println("Текущие дата и время после форматирования: " + currentDateTimeFormat);
+        Item item2 = new Item();
+        System.out.println(item2);
     }
 }

ed93a8f83f1f898ddfdca4bd7470eb496201bc18
Kate Stulova <stulova.ek@gmail.com>
2021-11-23 13:07:19 +0300

    improvement

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -56,9 +56,10 @@ public class Tracker {
     }
 
     public boolean delete(int id) {
-        boolean res = indexOf(id) != -1;
+        int index = indexOf(id);
+        boolean res = index != -1;
         if (res) {
-            System.arraycopy(items, indexOf(id) + 1, items, indexOf(id), size - indexOf(id) - 1);
+            System.arraycopy(items, index + 1, items, index, size - index - 1);
             items[size - 1] = null;
             size--;
         }

d64948e747d0c586e617aa493512aead4b906aac
Kate Stulova <stulova.ek@gmail.com>
2022-02-03 20:16:56 +0300

    improvement

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -6,6 +6,8 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("stulova.ek@gmail.com", "Stulova");
+        map.put("stulova.ek@gmail.com", "Stulova");
+        map.put("stulova.ek@gmail.com", "Stulova");
         for (String key : map.keySet()) {
             String value = map.get(key);
             System.out.println(value);

ba25df873a144cb0c9690c7e79f53debd718f8c3
Kate Stulova <stulova.ek@gmail.com>
2022-02-13 21:01:03 +0300

    0. Сортировка [#10096]

---
+++ src/test/java/ru/job4j/tracker/ItemAscByNameTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class ItemAscByNameTest {
+    @Test
+    public void sortAscByName() {
+        List<Item> items = Arrays.asList(
+                new Item(1, "Kate"),
+                new Item(2, "One more Kate"),
+                new Item(3, "And one more Kate")
+        );
+        List<Item> expected = Arrays.asList(
+                new Item(3, "And one more Kate"),
+                new Item(1, "Kate"),
+                new Item(2, "One more Kate")
+        );
+        Collections.sort(items, new ItemAscByName());
+        assertThat(items, is(expected));
+    }
+}

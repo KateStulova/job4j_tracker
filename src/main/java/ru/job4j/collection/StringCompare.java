92b3ad1bb97ae6935f595af74dec76b64e43e782
Kate Stulova <stulova.ek@gmail.com>
2022-02-25 18:32:30 +0300

    improvement

--- src/main/java/ru/job4j/collection/StringCompare.java
+++ src/main/java/ru/job4j/collection/StringCompare.java
@@ -5,43 +5,14 @@ import java.util.Comparator;
 public class StringCompare implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
-        int rsl = 0;
-        int leftLength = left.length();
-        int rightLength = right.length();
-        int whatIsShorter = Integer.compare(leftLength, rightLength);
-        if (whatIsShorter < 0) {
-            for (int i = 0; i < leftLength; i++) {
-                int innerRsl = Character.compare(left.charAt(i), right.charAt(i));
-                if (innerRsl != 0) {
-                    rsl = innerRsl;
-                    break;
-                }
-                if (i == leftLength - 1) {
-                    rsl = leftLength - rightLength;
-                    break;
-                }
-            }
-        } else if (whatIsShorter > 0) {
-            for (int i = 0; i < rightLength; i++) {
-                int innerRsl = Character.compare(left.charAt(i), right.charAt(i));
-                if (innerRsl != 0) {
-                    rsl = innerRsl;
-                    break;
-                }
-                if (i == rightLength - 1) {
-                    rsl = leftLength - rightLength;
-                    break;
-                }
-            }
-        } else {
-            for (int i = 0; i < leftLength; i++) {
-                int innerRsl = Character.compare(left.charAt(i), right.charAt(i));
-                if (innerRsl != 0) {
-                    rsl = innerRsl;
-                    break;
-                }
+        int minLength = Math.min(left.length(), right.length());
+        for (int i = 0; i < minLength; i++) {
+            int innerRsl = Character.compare(left.charAt(i), right.charAt(i));
+            if (innerRsl != 0) {
+                return innerRsl;
             }
         }
-        return rsl;
-        }
+        return Integer.compare(left.length(), right.length());
     }
+}
+

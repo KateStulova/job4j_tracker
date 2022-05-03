b424ff7d67273caf280dc9048e6a583def515462
Kate Stulova <stulova.ek@gmail.com>
2022-05-03 16:53:40 +0400

    improvement

--- src/main/java/ru/job4j/stream/School.java
+++ src/main/java/ru/job4j/stream/School.java
@@ -6,8 +6,8 @@ import java.util.stream.Collectors;
 
 public class School {
     public List<Student> collect(List<Student> students, Predicate<Student> predict) {
-        return students.stream().
-                filter(predict)
+        return students.stream()
+                .filter(predict)
                 .collect(Collectors.toList());
     }
 }
